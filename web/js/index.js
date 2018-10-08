86a37e0f5481c084f459f137ac893a7cc5025fa8
Ran Berenfeld <ranb@liveu.tv>
2018-10-08 13:16:35 +0300

    find topic on index select

--- web/js/index.js
+++ web/js/index.js
@@ -2,10 +2,20 @@
 
 var index = {};
 
+function index_search_topic(topic_name)
+{
+    location.href = "find_teachers?topic_name=" + encodeURIComponent(topic_name);
+}
+
 function index_find_teachers_submit()
 {
     var topic_name = $("#index_topic_name").val();
-    location.href = "find_teachers?topic_name=" + encodeURIComponent(topic_name);
+    index_search_topic(topic_name);
+}
+
+function index_topic_selected(event, ui)
+{
+    index_search_topic(ui.item.value);
 }
 
 function index_init()
@@ -17,7 +27,8 @@ function index_init()
         index.all_topics_names.push(topic.name);
     }
     $("#index_topic_name").autocomplete({
-        source: index.all_topics_names
+        source: index.all_topics_names,
+        select: index_topic_selected
     });
     $("#index_topic_name").keyup(
             function (event) {
